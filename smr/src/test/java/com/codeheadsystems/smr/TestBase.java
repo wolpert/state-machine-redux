2eca9d83b06bb50bf0e07a58c00cefb5d7c5512d
Ned Wolpert <ned.wolpert@gmail.com>
2024-08-19 07:41:54 -0700

    Tests of the state machien definition

--- smr/src/test/java/com/codeheadsystems/smr/TestBase.java
+++ smr/src/test/java/com/codeheadsystems/smr/TestBase.java
@@ -7,6 +7,7 @@ public class TestBase {
   public static final State ONE = ImmutableState.of("one");
   public static final State TWO = ImmutableState.of("two");
   public static final State THREE = ImmutableState.of("three");
+  public static final State FOUR = ImmutableState.of("four");
   public static final Event TO_TWO = ImmutableEvent.of("ToTwo");
   public static final Event TO_THREE = ImmutableEvent.of("ToThree");
   public static final Event TO_ONE = ImmutableEvent.of("ToOne");
---
+++ smr/src/test/java/com/codeheadsystems/smr/impl/StateMachineDefinitionTest.java
@@ -0,0 +1,55 @@
+package com.codeheadsystems.smr.impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.codeheadsystems.smr.StateMachine;
+import com.codeheadsystems.smr.TestBase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StateMachineDefinitionTest extends TestBase {
+
+  private StateMachineDefinition definition;
+
+  @BeforeEach
+  void setup() {
+    final StateMachineDefinitionBuilder<?> builder = builder(false);
+    definition = new StateMachineDefinition(builder);
+  }
+
+  @Test
+  void initialState() {
+    assertThat(definition.initialState()).isEqualTo(ONE);
+  }
+
+  @Test
+  void states() {
+    assertThat(definition.states()).containsExactlyInAnyOrder(ONE, TWO, THREE);
+  }
+
+  @Test
+  void events() {
+    assertThat(definition.events(ONE)).containsExactlyInAnyOrder(TO_TWO);
+    assertThat(definition.events(TWO)).containsExactlyInAnyOrder(TO_THREE, TO_ONE);
+    assertThat(definition.events(THREE)).containsExactlyInAnyOrder(TO_TWO);
+  }
+
+  @Test
+  void hasState() {
+    assertThat(definition.hasState(ONE)).isTrue();
+    assertThat(definition.hasState(TWO)).isTrue();
+    assertThat(definition.hasState(THREE)).isTrue();
+    assertThat(definition.hasState(FOUR)).isFalse();
+  }
+
+  @Test
+  void forEvent() {
+    assertThat(definition.forEvent(ONE, TO_TWO)).contains(TWO);
+    assertThat(definition.forEvent(ONE, TO_THREE)).isEmpty();
+    assertThat(definition.forEvent(TWO, TO_THREE)).contains(THREE);
+    assertThat(definition.forEvent(TWO, TO_ONE)).contains(ONE);
+    assertThat(definition.forEvent(THREE, TO_TWO)).contains(TWO);
+    assertThat(definition.forEvent(THREE, TO_ONE)).isEmpty();
+  }
+
+}
