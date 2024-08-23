4b33d4007537e6ed5361f2c206f2d0c55096eb85
Ned Wolpert <ned.wolpert@gmail.com>
2024-08-23 08:06:37 -0700

    Added publishing

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
+}
+nexusPublishing {
+    repositories {
+        sonatype()
+    }
+}
+allprojects {
+    group = "com.codeheadsystems"
+    version = "1.0.1-SNAPSHOT"
+}
+
+// gradle clean build test publishToSonatype closeAndReleaseSonatypeStagingRepository
--- smr/build.gradle.kts
+++ smr/build.gradle.kts
@@ -8,6 +8,8 @@
 plugins {
     // Apply the java-library plugin for API and implementation separation.
     `java-library`
+    `maven-publish`
+    signing
 }
 
 repositories {
@@ -33,9 +35,62 @@ java {
     toolchain {
         languageVersion = JavaLanguageVersion.of(11)
     }
+    withJavadocJar()
+    withSourcesJar()
 }
 
 tasks.named<Test>("test") {
     // Use JUnit Platform for unit tests.
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "smr"
+            from(components["java"])
+            pom {
+                name = "State Machine Redux"
+                description = "Core Feature Flag library"
+                url = "https://github.com/wolpert/state_machine_redux"
+                licenses {
+                    license {
+                        name = "The Apache License, Version 2.0"
+                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+                    }
+                }
+                developers {
+                    developer {
+                        id = "wolpert"
+                        name = "Ned Wolpert"
+                        email = "ned.wolpert@gmail.com"
+                    }
+                }
+                scm {
+                    connection = "scm:git:git://github.com/wolpert/state_machine_redux.git"
+                    developerConnection = "scm:git:ssh://github.com/wolpert/state_machine_redux.git"
+                    url = "https://github.com/wolpert/state_machine_redux/"
+                }
+            }
+
+        }
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2"
+            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots"
+            url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
+            name = "ossrh"
+            credentials(PasswordCredentials::class)
+        }
+    }
+}
+signing {
+    useGpgCmd()
+    sign(publishing.publications["mavenJava"])
+}
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
 }
